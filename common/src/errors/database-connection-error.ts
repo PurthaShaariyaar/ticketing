d8b1684afab6db5b542df1beff0a681f74e9bf2f
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-10-29 23:28:27 -0400

    Updates

--- common/src/errors/database-connection-error.ts
+++ common/src/errors/database-connection-error.ts
@@ -4,7 +4,7 @@ import { CustomError } from "./custom-error";
 export class DatabaseConnectionError extends CustomError {
 
   statusCode = 500;
-  reason = 'Error connecting to database.';
+  reason = 'Error connecting to database!';
 
   constructor() {
     super('Error connecting to database.');
